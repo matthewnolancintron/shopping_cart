ace88e2b5901aa2bd19a00eb739a74c96007949e
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-09 14:41:02 -0500

    added comments to some of the files from create react app that I will work on later

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+/**
+ * Make sure to test your app thoroughly
+ *  using the React Testing Library. 
+ * Take care that you don’t 
+ * directly test react-router-dom, 
+ * since it is an external library and the 
+ * developers working on it must have tested 
+ * the library already.
+ */
