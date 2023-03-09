db5e93d68b11ed802aec831c5dc592ea9753a89e
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-09 14:56:06 -0500

    started working on ShopPage comopnent

---
+++ src/components/ShopPage.jsx
@@ -0,0 +1,19 @@
+/**
+ * includes a shopping cart
+ * add productCard components to page
+ * 
+ * Once a user has submitted their order,
+ * the amount on the cart itself should adjust.
+ */
+
+import NavigationBar from "./NavigationBar";
+
+export default function ShopPage(){
+    return(
+        <section>
+            <NavigationBar/>
+            <h1>Shop Page</h1>
+
+        </section>    
+    );
+}
