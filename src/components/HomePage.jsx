7df020e0f1d341e31e8a278006526972ddc481d5
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-09 15:42:50 -0500

    removed uneeded notes from HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,3 @@
-/**
- * todo: add navigation component to home page
- * add imgs or just some basic info doesn't matter
- * 
- * main goal of the project is to focus
- * on setting up the shopping cart.
- * 
- * The homepage is there to practice routing 
- * using react-router-dom.
- */
-
 import NavigationBar from "./NavigationBar";
 
 export default function HomePage(){
