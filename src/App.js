14b2d62a2c763bca5b0bbd8f1df6eb9704874575
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-10 18:09:46 -0500

    imported CartProvider from CartContext and wrapped it around BrowserRouter for each route/component to have access to the data from CartContext

--- src/App.js
+++ src/App.js
@@ -10,19 +10,22 @@
  */
 
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { CartProvider } from './CartContext';
 import HomePage from './components/HomePage';
 import ShopPage from './components/ShopPage';
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
-    <BrowserRouter className="App">
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/Shop" element={<ShopPage/>} />
-        <Route path="/Cart" element={<ShoppingCart/>}/>
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter className="App">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/Shop" element={<ShopPage/>}/>
+          <Route path="/Cart" element={<ShoppingCart/>}/>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
