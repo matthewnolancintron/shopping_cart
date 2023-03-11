f2d1283b316725a2844e2b670d378ca1c1bdd1de
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-11 16:53:42 -0500

    added render list component of itemsInCartCard component to ShoppingCart component or Cart Page

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -6,16 +6,25 @@
 import React from 'react'; 
 import { CartContext } from '../CartContext';
 import NavigationBar from "./NavigationBar";
+import ItemInCartCard from "./ItemInCartCard";
+
 
 export default function ShoppingCart(){
     const { itemsInCart } = React.useContext(CartContext);
+    const itemsInCartComponentList = itemsInCart.map((item)=>(
+        <li key={item.id}>
+            <ItemInCartCard productInCart={item}/>
+        </li>
+    ));
+
     return(
         <section>
         <NavigationBar/>
         <h1>cart</h1>
-        {itemsInCart.map((item)=>{
-            /**todo create itemInCartProductCard component */
-        })}
+        <ul>
+        {itemsInCart.length == 0 ? <h1>you have no items in your cart</h1> : itemsInCartComponentList
+        } 
+        </ul>
         </section>
     );
 }
