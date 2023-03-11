86f806d751166694d687a3076feb179e2a72bf1d
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-03-11 15:55:58 -0500

    rather than adding duplicate objects to the cart, if not already present  a quanity prop is added and updated instead

--- src/CartContext.js
+++ src/CartContext.js
@@ -5,13 +5,47 @@ export const CartContext = createContext();
 export const CartProvider = (props) => {
   const [itemsInCart, setItemsInCart] = useState([]);
 
-  const addToCart = (newItem,quantity) => {
-    if(quantity > 1){
-      const ItemsBeingAdded = Array.from({ length: quantity }, () => Object.assign({}, newItem));
-      setItemsInCart([...itemsInCart, ...ItemsBeingAdded]);
+  const addToCart = (newItem, quantity) => {
+    const isItemAlreadyInCart = itemsInCart.map(item => item.id).includes(newItem.id);
+    const updateQuantityOfItemAlreadyInCart = (itemToUpdate, amount) => {
+      // get index of the item being added in the current cart
+      const indexOfnewItemInCart = itemsInCart.map(item => item.id).indexOf(itemToUpdate.id);
+
+      // console.log(itemsInCart[indexOfnewItemInCart].hasOwnProperty('quantity'));
+
+      // Create a new object that has the updated `quantity` key-value pair.
+      const updatedObjectQuanitity = {
+        ...itemsInCart[indexOfnewItemInCart],  // copy all the key-value pairs from the original object
+        quantity: itemsInCart[indexOfnewItemInCart].hasOwnProperty('quantity') ?
+          parseInt(itemsInCart[indexOfnewItemInCart].quantity) + parseInt(amount) : parseInt(amount) + 1 // update the `quantity` key-value pair
+      };
+
+      // Create a new array that contains all the objects from the old state array.
+      const itemsInCartCopy = [...itemsInCart];
+      
+      // Replace the old object at that index with the new object.
+      itemsInCartCopy[indexOfnewItemInCart] = updatedObjectQuanitity;
+      //
+      setItemsInCart(itemsInCartCopy);
+    };
+
+    if (isItemAlreadyInCart) {
+      updateQuantityOfItemAlreadyInCart(newItem, quantity);
     } else {
-      setItemsInCart([...itemsInCart, newItem]);
+      if (quantity > 1) {
+        // Create a new object that has the updated `quantity` key-value pair.
+        const itemWithAddedQuanityProp = {
+          ...newItem,  // copy all the key-value pairs from the original object
+          quantity: parseInt(quantity) // update the `quantity` key-value pair
+        };
+        setItemsInCart([...itemsInCart, itemWithAddedQuanityProp]);
+
+      } else {
+        setItemsInCart([...itemsInCart, newItem]);
+      }
+
     }
+
   };
 
   return (
